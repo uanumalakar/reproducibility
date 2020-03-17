9500f7fd60ca19878312f7713b63de15cafe93bb
Malakar, Yuwan (L&W, Dutton Park) <Yuwan.Malakar@csiro.au>
2020-03-17 10:24:59 +1000

    challenge 2

--- codes/reproducibility_with_R.R
+++ codes/reproducibility_with_R.R
@@ -8,7 +8,7 @@ BOM_d <- read_csv("data/BOM_data.csv")
 
 BOM_s <- read_csv("data/BOM_stations.csv")
 
-
+#CHALLENGE 1
 #create a dataset by grouping station_numbers and filtering with all the values except "-/-" within the variable Temp_min_max
 BOM_d_temp <- BOM_d %>% 
   group_by(Station_number) %>% 
@@ -22,3 +22,16 @@ BOM_d_temp_rain <- filter(BOM_d_temp_sep, Temp_min !="-" & Temp_max !="-" & Rain
 Temp_rain_days <- group_by(BOM_d_temp_rain, Station_number) %>% 
   summarise(days=n())
 
+#CHALLENGE 2
+
+#Average temp by month
+BOM_d_temp_rain$Temp_min <- as.numeric(BOM_d_temp_rain$Temp_min)
+BOM_d_temp_rain$Temp_max <- as.numeric(BOM_d_temp_rain$Temp_max)
+BOM_d_temp_rain$Rainfall <- as.numeric(BOM_d_temp_rain$Rainfall)
+
+Average_month_temp_rain <- BOM_d_temp_rain %>% 
+  group_by(Month) %>% 
+  summarise(mean_min_Temp=mean(Temp_min), mean_max_Temp=mean(Temp_max), mean_Rainfall=mean(Rainfall))
+
+Temp_diff <- mutate(Average_month_temp_rain, Temp_diff=mean_max_Temp-mean_min_Temp) %>% 
+  arrange(Temp_diff)
